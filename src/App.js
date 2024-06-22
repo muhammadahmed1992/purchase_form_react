980e187d2e3e07290897b90da51443cd2cc713c7
Muhammad Ahmedm <muhammed_hm@hotmail.com>
2024-06-22 15:30:01 +0500

    Fix page size to A4

--- src/App.js
+++ src/App.js
@@ -32,7 +32,10 @@ function PurchaseOrderHeader() {
       const input = printRef.current;
       const canvas = await html2canvas(input);
       const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF();
+      const pdf = new jsPDF({
+        unit: 'mm',
+        format: 'a4'
+      });
       const imgProps = pdf.getImageProperties(imgData);
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
